f8f74ffb28ba06d40a17e47e572cb2efbdf4fd1b
Lucas-Bispo <lukaobispo@gmail.com>
2022-09-07 20:43:08 -0300

    :construction: Trabalho em progresso.

---
+++ Genius.java
@@ -0,0 +1,27 @@
+// Classe que encapsula a fucao principal
+
+import java.awt.event.*;
+
+public class Genius {
+
+    public static void main(String[] args){
+        // Cria a instacia de Jogo
+        Jogo jogo = new Jogo();
+
+        // chama a funcao que monta a tela de abertura 
+        jogo.tela_abertura =();
+
+        // evento que finaliza o programa quando clicar no x do programa
+        jogo.addWindowListener(
+            new WindowAdapter() {
+                public void windowClosed(WindowEvent windowEvent) {
+                    // encerra o aplicativo
+                    System.exit(status: 0);
+                }
+
+            }
+
+        );
+
+    }
+}
