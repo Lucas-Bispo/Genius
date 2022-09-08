72cae8ccb6a803b5325b93ff89441e4dd68b96ac
Lucas-Bispo <lukaobispo@gmail.com>
2022-09-07 21:18:47 -0300

    :construction: Trabalho em progresso.

--- Botao.java
+++ Botao.java
@@ -1,3 +1,40 @@
-public class Botao {
+import javax.swing.*;
+
+class Botao extends JButton {
+    private static final long serialVersionUID = 1L;
+    // atribuindo ultilizado para saber se o botao pode ser clicado
+    private boolean status;
+    // atribuindo ultilizado para facilitar o uso imagens e musicas
+    private String nome;
+
+    // criar o botao com o construtor do JButton
+    public Botao(String nome) {
+        super(new ImageIcon(Jogo.getImage(nome+"_apagdo.jpg")));
+        //indica a imagem do botao quando estiver sob o mause
+        setRolloverIcon(new ImageIcon(Jogo.getImage(nome+"_mercado.jpg")));
+        // remove a borda do botao
+        setBorder(BorderFactory.createEmptyBorder(top:0, left:0, bottom:0, right:0));
+        this.nome = nome;
+        
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+
+    }
+
+    public boolean getStatus() {
+        return (this.status);
+    }
+
+    public String getNome() {
+        return (this.nome);
+    }
+
+    // altera as imagens do botao recebido por paramentro para acesso
+    public void acenderBotao(Botao botao) {
+        botao.setIcon(new ImageIcon(Jogo.getImage(Botao.getNome()+"_aceso.jpg"));
+        botao.setRolloverIcon(new ImageIcon(Jogo.getImage(Botao.getNome()+"_aceso.jpg")));
+    }
 
 }
